5c39c98bdbe05d60df631c8fa8a0ece8c208d201
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-06-20 16:38:58 +0530

    Recursive Function to print names

---
+++ src/tuf/Recursion/PrintNamesNTimes.java
@@ -0,0 +1,24 @@
+package tuf.Recursion;
+
+import java.util.Scanner;
+
+public class PrintNamesNTimes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter how many times you want to print Ram:");
+        int size = sc.nextInt();
+
+
+        recursiveFunctionPrintRam(size);
+    }
+
+    private static void recursiveFunctionPrintRam(int size) {
+        if(size==1){
+            return;
+        }
+
+        recursiveFunctionPrintRam(size-1);
+
+        System.out.println("Ram Ji");
+    }
+}
