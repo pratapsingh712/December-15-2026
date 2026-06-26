27af99c61e6edbda3806b56b9dfd50fd71e40189
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-06-26 19:17:56 +0530

    second week arrayI

--- src/tuf/Arrays/Easy/SecondLargest.java
+++ src/tuf/Arrays/Easy/SecondLargest.java
@@ -3,28 +3,30 @@ package tuf.Arrays.Easy;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class HighestElement {
+public class SecondLargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of your array :");
         int size = sc.nextInt();
         int[] array = new int[size];
 
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             array[i] = sc.nextInt();
         }
 
         int max = array[0];
+        int secondMax = array[0];
 
-        System.out.println("Maximum element in the array is :"+getMaximum(array,max));
+        System.out.println("Second highest element in the array is :" + secondHighestElement(array, max, secondMax));
     }
 
-    private static int getMaximum(int[] array, int max) {
-        for(int i=1;i<array.length;i++){
-            if(array[i]>max){
+    private static int secondHighestElement(int[] array, int max, int secondMax) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                secondMax = max;
                 max = array[i];
             }
         }
-        return max;
+        return secondMax;
     }
 }
